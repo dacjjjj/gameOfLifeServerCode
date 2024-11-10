0dbab6c2794fb30bf79ef148b5c55a6a267ba211
dom <100240196+dacjjjj@users.noreply.github.com>
2024-11-10 16:45:52 +0000

    Add files via upload

---
+++ gol-skeleton-master/gol/stubs/stubs.go
@@ -0,0 +1,31 @@
+package stubs
+
+import "uk.ac.bris.cs/gameoflife/util"
+
+var StartMaster = "GolMasterRunner.MasterStart"
+var StartWorker = "GameOfLifeOperations.ProcessGameOfLife"
+
+type Response struct {
+	WorkerNumber   int
+	FinalWorld     [][]byte
+	TurnsCompleted int
+}
+
+type Request struct {
+	WorkerNumber int
+	NextWorld    [][]byte
+	Turns        int
+	ThreadCount  int
+}
+
+type FinalResponse struct {
+	FinalWorld     [][]byte
+	TurnsCompleted int
+	AliveCells     []util.Cell
+}
+
+type InitialRequest struct {
+	NextWorld   [][]byte
+	Turns       int
+	ThreadCount int
+}
